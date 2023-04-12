32e77830132e430e6fb3e7df916d430731a648e1
Tamanna Saini <tamanna999saini@gmail.com>
2023-04-11 23:41:41 -0700

    Double jump done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,36 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
 
-    private Rigidbody rb;
+	// Create public variables for player speed, and for the Text UI game objects
+	public float speed;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
 
-    private float movementX;
-    private float movementY;
+	private float movementX;
+	private float movementY;
+	private float movementZ;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private Rigidbody rb;
+	private int count;
 
-    private void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+	// At the start of the game..
+	void Start()
+	{
+		// Assign the Rigidbody component to our private rb variable
+		rb = GetComponent<Rigidbody>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+		// Set the count to zero 
+		count = 0;
 
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+		SetCountText();
 
-        rb.AddForce(movement * speed);
-    }
+		// Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
+		winTextObject.SetActive(false);
+	}
+
+	void FixedUpdate()
+	{
+		// Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
+		Vector3 movement = new Vector3(movementX, movementZ, movementY);
+
+		rb.AddForce(movement * speed);
+
+		movementZ = 0.0f;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		// ..and if the GameObject you intersect has the tag 'Pick Up' assigned to it..
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+
+			// Add one to the score variable 'count'
+			count = count + 1;
+
+			// Run the 'SetCountText()' function (see below)
+			SetCountText();
+		}
+	}
+
+	void OnMove(InputValue value)
+	{
+		Vector2 v = value.Get<Vector2>();
+
+		movementX = v.x;
+		movementY = v.y;
+	}
+
+	void OnJump(InputValue value)
+	{
+		//Debug.Log(transform.position.y);
+		if (transform.position.y < 3.0)
+		{
+			movementZ = 40.0f;
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 12)
+		{
+			// Set the text value of your 'winText'
+			winTextObject.SetActive(true);
+		}
+	}
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Roll-a-Ball
+This was done for CS 410/510 Game programming.
+
+
+## Author
+Tamanna Saini
